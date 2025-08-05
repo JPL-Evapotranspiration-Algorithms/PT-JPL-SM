a84a409105452aaf166bcca8de9b5fff21faf405
Gregory H. Halverson <gregory.h.halverson@jpl.nasa.gov>
2025-08-05 10:30:06 -0700

    v1.6.0 fixing verification after PM-JPL fwet

--- PTJPLSM/__init__.py
+++ PTJPLSM/__init__.py
@@ -1,9 +1,3 @@
 from .PTJPLSM import *
 
-from os.path import join, dirname, abspath
-
-with open(join(abspath(dirname(__file__)), "version.txt")) as f:
-    version = f.read()
-
-__version__ = version
 __author__ = "Gregory H. Halverson"
